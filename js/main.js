610e1ba75855ca2c1ee874de4be7d62e9152a259
lukasz ciepichal <lukaszciepichal1@gmail.com>
2021-11-09 12:43:43 +0100

    dodanie funkcji

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 
 let liczba = 300
 
-if (liczba >= 100 && liczba <= 200){
-    console.log('Liczba znajduje się w przedziale');
-} else {
-    console.log('Liczba nie znajduje się w przedziale');
+function przedzial(liczba){
+    if (liczba >= 100 && liczba <= 200){
+        console.log('Liczba znajduje się w przedziale');
+    } else {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
 }
+
+przedzial(liczba)
